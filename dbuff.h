afc597478a864158e7386d87f54cba298ad0503c
fluro_red_fox <fluroredfox@gmail.com>
2025-11-05 01:53:42 +1100

    first implimentation

--- dbuff.h
+++ dbuff.h
@@ -3,8 +3,11 @@
 
 #include <stdint.h>
 
+// empty dbuff object.
 #define DBUFF_EMPTY ((dbuff_t) {NULL, 0, 0})
 
+/* dynamic buffer.
+values should not be directly modified.*/
 typedef struct dbuff_t {
     void *buffer;
     size_t used;
@@ -12,8 +15,24 @@ typedef struct dbuff_t {
 }
 dbuff_t;
 
+// frees memory used by dbuff.
 void dbuff_free(dbuff_t *dbuff);
 
-void dbuff_
+// get the addres of the first free byte in 'dbuff'.
+void *dbuff_get_top(const dbuff_t *dbuff);
+
+/* reserves at least 'size' free bytes in 'dbuff'.
+returns the same as 'dbuff_get_top' after reallocation.*/
+void *dbuff_allocate(dbuff_t *dbuff, size_t size);
+
+// removes unused space in 'dbuff'.
+void dbuff_compact(dbuff_t *dbuff);
+
+// appends 'size' bytes from 'item' onto 'dbuff', allocating space if necessary.
+void dbuff_push(dbuff_t *dbuff, const void *item, size_t size);
+
+/* removes 'size' bytes from 'dbuff', copies them to 'item' if not NULL.
+does not reallocate. */
+void dbuff_pop(dbuff_t *dbuff, void *item, size_t size);
 
 #endif
